5dcc7e266a67bc290c470c7289c9b6f501e15901
zainabnazari <42781686+zainabnazari@users.noreply.github.com>
2020-04-01 19:44:38 +0200

    Update cineca_marconi.modules.sh
    unwanted space between module's name removed.

--- InstallNotes/MakeDefsLocalExamples/cineca_marconi.modules.sh
+++ InstallNotes/MakeDefsLocalExamples/cineca_marconi.modules.sh
@@ -4,5 +4,5 @@ module load intel/pe-xe-2018--binary
 module load intelmpi/2018--binary
 module load zlib/1.2.8--gnu--6.1.0
 module load hdf5/1.8.18--intelmpi--2018--binary
-module load gnu /7.3.0
+module load gnu/7.3.0
 module load mkl/2018--binary
